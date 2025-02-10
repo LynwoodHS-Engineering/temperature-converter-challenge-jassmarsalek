352d17768ffda141e53476bf6f4e6ea5842b9a0a
jassmarsalek <jusmar3799@stu.mylusd.org>
2025-02-10 11:16:20 -0800

    temperatureconverter.py

---
+++ temperatureconverter.py
@@ -0,0 +1,12 @@
+def kelvin_to_celsius(temp):
+  return (temp)-273.15
+def farhenheint_to_celsius(temp):
+  return (temp-32)* (5/9)
+def kelvin_to_farhenheint(temp):
+  return (temp-273.15)* (9/5)+32
+def farhenheint_to_kelvin(temp):
+  return (temp-32)*(5/9)+273.15
+def celsius_to_kelvin(temp):
+  return (temp)+273.15
+def celsius_farhenheint(temp):
+  return (temp)*(9/5)+32
